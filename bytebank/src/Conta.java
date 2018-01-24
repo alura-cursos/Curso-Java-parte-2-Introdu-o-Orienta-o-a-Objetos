2720be756cbe3b76613a30ce7c5e0e8eeb6df75b
Andre Chaves <andrechavesg@gmail.com>
2018-01-24 18:37:55 -0200

    capitulo3.

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -1,8 +1,29 @@
-public class Conta{
+public class Conta {
 
     double saldo;
     int agencia;
     int numero;
     String titular;
 
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
---
+++ bytebank/src/TestaMetodo.java
@@ -0,0 +1,28 @@
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+
+		boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+
+		if (sucessoTransferencia) {
+			System.out.println("transferencia com sucesso");
+
+		} else {
+			System.out.println("faltou dinheiro");
+
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+	}
+
+}
